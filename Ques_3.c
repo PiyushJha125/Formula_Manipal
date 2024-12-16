ccb0637db7af6eb16bcc21ba1ed7c60557f6a26e
Piyush Jha <piyush.jha1357@gmail.com>
2024-12-16 22:54:13 +0530

    Update Ques_3.c

--- Ques_3.c
+++ Ques_3.c
@@ -1,3 +1,4 @@
+//Create a program to calculate electricity bills based on usage slabs (e.g.if total units are 350 then 0–100 units: ₹5/unit, 101–300 units: ₹8/unit, ₹10/unit for units above 300, then total bill is of 350 units is 2900).
 #include <stdio.h>
 void main() 
 {
